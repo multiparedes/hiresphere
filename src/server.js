379e22af3274aab279e9f95e92f58895fcb40b9a
multiparedes <martiparedes19@gmail.com>
2024-12-27 19:42:47 +0100

    ESlint prettier

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
-import express from 'express';
+import express from 'express'
 import 'dotenv/config'
 
 // Env file variables
-const PORT = process.env.LOCAL_PORT || 8080;
+const PORT = process.env.LOCAL_PORT || 8080
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.listen(PORT, () => {
-    console.log(`Server started on port 8080`);
-});
+  console.log(`Server started on port 8080`)
+})
 
+app.get('/health', (req, res) => {
+  return res.status(200).send({
+    status: 'Ok',
+    uptime: process.uptime(),
+    date: new Date()
+  })
+})
